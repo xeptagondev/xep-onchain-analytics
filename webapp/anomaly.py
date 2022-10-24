e41c965411363dc80e6a570dbea2b9d9cfc62809
tammygrq <tammygrq@gmail.com>
2022-10-24 06:12:39 +0000

    update as of 24/10

--- webapp/anomaly.py
+++ webapp/anomaly.py
@@ -29,7 +29,10 @@ psqlcursor = psqlconn.cursor()
 
 # Dataframe for output of Anomaly Detection
 df_isoForest = pd.read_sql('SELECT * FROM "isoForest_outliers"', psqlconn)
+df_autoEncoder = pd.read_sql('SELECT * FROM "autoEncoder_outliers"', psqlconn)
 df_kmeans = pd.read_sql('SELECT * FROM "kmeans_outliers"', psqlconn)
+df_kmeans["anomaly"] = df_kmeans["anomaly"].astype(str)
+df_kmeans["cluster"] = df_kmeans["cluster"].astype(str)
 
 content = html.Div([
     dbc.Row([
@@ -71,6 +74,7 @@ content = html.Div([
                         dbc.AccordionItem(
                             dbc.ListGroup([
                                 dbc.ListGroupItem("Isolation Forest", action=True, id='outlier-isoForest', color='#E8EBEE00'),
+                                dbc.ListGroupItem("Auto-Encoders", action=True, id='outlier-autoEncoder', color='#E8EBEE00'),
                                 dbc.ListGroupItem("K-Means Clustering", action=True, id='outlier-kmeans', color='#E8EBEE00')
                             ], flush=True, style={'font-size':'14px'}),
                             
@@ -87,17 +91,17 @@ content = html.Div([
             dbc.Col([
                 html.Div([
                     html.H4(id="anomaly-title", style = {'display':'inline-block', 'vertical-align':'center'}),
-                    html.Div([
-                            html.Span("Select your preferred date range.", className='date-picker-text', style = {'font-size':'12px'}),
-                                dcc.DatePickerRange(
-                                id='my-date-picker-range2',
-                                clearable = True,
-                                min_date_allowed=date(2009, 1, 3),
-                                max_date_allowed=datetime.now(),
-                                start_date_placeholder_text='MM/DD/YY',
-                                end_date_placeholder_text='MM/DD/YY',
-                            ),
-                        ], className='date-picker-div', style = {'display':'inline-block', 'position': 'relative', 'float':'right', 'margin-top':'13px'})
+                    # html.Div([
+                    #         html.Span("Select your preferred date range.", className='date-picker-text', style = {'font-size':'12px'}),
+                    #             dcc.DatePickerRange(
+                    #             id='my-date-picker-range2',
+                    #             clearable = True,
+                    #             min_date_allowed=date(2009, 1, 3),
+                    #             max_date_allowed=datetime.now(),
+                    #             start_date_placeholder_text='MM/DD/YY',
+                    #             end_date_placeholder_text='MM/DD/YY',
+                    #         ),
+                    #     ], className='date-picker-div', style = {'display':'inline-block', 'position': 'relative', 'float':'right', 'margin-top':'13px'})
                     ]),
                 html.Div([
                     # dbc.Button('ALL', id="range-all-button", 
@@ -143,12 +147,16 @@ def update_dropdown(n1, n2, n3):
 @app.callback(
     Output('anomaly-title', "children"),
     [Input("anomaly-table", "n_clicks"), Input("anomaly-blocks", "n_clicks"), Input("anomaly-trans", "n_clicks"), 
-    Input("outlier-isoForest", "n_clicks"), Input("outlier-kmeans", "n_clicks")]
+    Input("outlier-isoForest", "n_clicks"), Input("outlier-autoEncoder", "n_clicks"), Input("outlier-kmeans", "n_clicks")]
 )
 
-def update_title(n1,n2,n3,n4,n5):
-    titles_dict = {"anomaly-table": "Table of Anomalous Transactions", "anomaly-blocks": "Anomalous Blocks Across Time", "anomaly-trans": "Anomalous Transactions Across Time",
-                   "outlier-isoForest": "Outliers Detected Using Isolation Forest", "outlier-kmeans": "Outliers Detected Using K-Means Clustering"}
+def update_title(n1,n2,n3,n4,n5,n6):
+    titles_dict = {"anomaly-table": "Table of Anomalous Transactions", 
+                   "anomaly-blocks": "Anomalous Blocks Across Time", 
+                   "anomaly-trans": "Anomalous Transactions Across Time",
+                   "outlier-isoForest": "Outliers Detected Using Isolation Forest", 
+                   "outlier-autoEncoder": "Outliers Detected Using Auto-Encoders", 
+                   "outlier-kmeans": "Outliers Detected Using K-Means Clustering"}
     if not ctx.triggered: #default
         return "Overview"
     selected = ctx.triggered[0]["prop_id"].split(".")[0]
@@ -157,85 +165,92 @@ def update_title(n1,n2,n3,n4,n5):
 # standalone method to reduce repeated chunks in callback below ##
 def create_fig(df, model):
     graphs = []
-    default = dict(rangeselector=dict(buttons=list([
-                dict(count=1,
-                    label="1M",
-                    step="month",
-                    stepmode="backward"),
-                dict(count=6,
-                    label="6M",
-                    step="month",
-                    stepmode="backward"),
-                dict(count=1,
-                    label="YTD",
-                    step="year",
-                    stepmode="todate"),
-                dict(count=1,
-                    label="1Y",
-                    step="year",
-                    stepmode="backward"),
-                dict(label="ALL", step="all")
-                ])
-            ),
-            rangeslider=dict(
-                visible=True
-            ),
-            type="date"
-        )
-    non_features = ['Date', 'index', 'anomaly_IsoForest', 'anomaly_kmeans']
-    features = set(df.columns) - set(non_features)
+    default = dict(rangeslider=dict(visible=True),type="date")
+
+    non_features = ['Date', 'index', 'anomaly', 'score']
+    features = sorted(set(df.columns) - set(non_features))
     for c in features:
         fig1 = px.line(df, x="Date", y=c, color_discrete_sequence=["#0a275c"])
-        outlier = df.loc[df['anomaly_kmeans'] == 1] if model == 'kmeans' else df.loc[df['anomaly_IsoForest'] == 1]
+        outlier = df.loc[df['anomaly'] == 1]
         fig2 = px.scatter(outlier, x="Date", y=c, color_discrete_sequence=["firebrick"])
         fig = go.Figure(data = fig1.data + fig2.data)
-        fig.update_xaxes(default) #adding in default range selectors & slider
-        # setting date range limit to fix bug with range selector
+        fig.update_xaxes(default) # adding in default range slider
+        # setting date range limit to fix range slider bug with scatter plots
         fig.update_xaxes(range=[df['Date'].iloc[0], df['Date'].iloc[-1]],
                         rangeslider_range=[df['Date'].iloc[0], df['Date'].iloc[-1]])
-        fig.update_xaxes(rangeselector_font_size = 15, title_text = "Date")
+        fig.update_xaxes(title_text = "Date")
         fig.update_yaxes(title_text = c)
-        fig.update_layout(plot_bgcolor='white', title_text = 'Plot of {} over Time'.format(c), title_x=0.5, title_font_color = 'black', title_font_size=14) 
+        fig.update_layout(plot_bgcolor='white', title_text = '{} over Time'.format(c), title_x=0.5, title_font_color = 'black', title_font_size=16) 
         graphs.append(dcc.Graph(id ='{}-{}'.format(model,c), figure = fig))
 
     return graphs
 
+def create_cluster(df, model):
+    graphs = []
+    # Visualising Clusters
+    fig = px.scatter(df, x="Principal Component 1", y="Principal Component 2", color="cluster", color_discrete_sequence=px.colors.qualitative.Prism)
+    fig.update_xaxes(title_text = "Principal Component 1")
+    fig.update_yaxes(title_text = "Principal Component 2")
+    fig.update_layout(plot_bgcolor='white', title_text = "Clusters detected using K-Means", title_x=0.5, title_font_color = 'black', title_font_size=16) 
+    graphs.append(dcc.Graph(id ='{}-{}'.format(model, "cluster"), figure = fig))
+
+    # Visualising Anomalies
+    fig = px.scatter(df, x="Principal Component 1", y="Principal Component 2", color="anomaly", color_discrete_sequence=["firebrick", "#0a275c"])
+    fig.update_xaxes(title_text = "Principal Component 1")
+    fig.update_yaxes(title_text = "Principal Component 2")
+    fig.update_layout(plot_bgcolor='white', title_text = "Outlier Detection using K-Means: Red represents Anomaly", title_x=0.5, title_font_color = 'black', title_font_size=16) 
+    graphs.append(dcc.Graph(id ='{}-{}'.format(model, "anomaly"), figure = fig))
+    
+    return graphs
+
 @app.callback(
     Output("outlier-graphs", "children"),
-    [Input("outlier-isoForest", "n_clicks"), Input("outlier-kmeans", "n_clicks")],
-    Input('my-date-picker-range2', 'start_date'),
-    Input('my-date-picker-range2', 'end_date'),
+    [Input("outlier-isoForest", "n_clicks"), 
+    Input("outlier-autoEncoder", "n_clicks"), 
+    Input("outlier-kmeans", "n_clicks")],
+    # Input('my-date-picker-range2', 'start_date'),
+    # Input('my-date-picker-range2', 'end_date'),
     Input('anomaly-title', 'children'),
     #Input('range-all-button', 'n_clicks')
 )
 
-def update_line_chart(iso, kmeans, start, end, curr_title):
+def update_line_chart(iso, autoEncoder, kmeans, curr_title):
     graphs = []
     if ctx.triggered[0]["prop_id"].split(".")[0] == 'outlier-isoForest':
         print("1st")             
         graphs = create_fig(df_isoForest, 'isoForest')
-    elif ctx.triggered[0]["prop_id"].split(".")[0] == 'outlier-kmeans':
+    elif ctx.triggered[0]["prop_id"].split(".")[0] == 'outlier-autoEncoder':
         print("2nd")
-        graphs = create_fig(df_kmeans, 'kmeans')
+        graphs = create_fig(df_autoEncoder, 'autoEncoder')
+    elif ctx.triggered[0]["prop_id"].split(".")[0] == 'outlier-kmeans':
+        print("3rd")
+        graphs = create_cluster(df_kmeans, 'kmeans')
     
-    #update graph based on date range selected by user
-    if start is not None and end is not None:
-        if 'isolation forest' in curr_title.lower():
-            print("3rd")
-            filtered_df = df_isoForest[df_isoForest['Date'].between(parse(start).date(), parse(end).date())]
-            graphs = create_fig(filtered_df, 'isoForest')
-        elif 'k-means' in curr_title.lower():
-            print("4th")
-            filtered_df = df_kmeans[df_kmeans['Date'].between(parse(start).date(), parse(end).date())]
-            graphs = create_fig(filtered_df, 'kmeans')
+    # # update graph based on date range selected by user
+    # if start is not None and end is not None:
+    #     if 'isolation forest' in curr_title.lower():
+    #         print("4th")
+    #         filtered_df = df_isoForest[df_isoForest['Date'].between(parse(start).date(), parse(end).date())]
+    #         graphs = create_fig(filtered_df, 'isoForest')
+    #     elif 'auto-encoder' in curr_title.lower():
+    #         print("5th")
+    #         filtered_df = df_autoEncoder[df_autoEncoder['Date'].between(parse(start).date(), parse(end).date())]
+    #         graphs = create_fig(filtered_df, 'autoEncoder')
+    #     elif 'k-means' in curr_title.lower():
+    #         print("6th")
+    #         filtered_df = df_kmeans[df_kmeans['Date'].between(parse(start).date(), parse(end).date())]
+    #         graphs = create_cluster(filtered_df, 'kmeans')
 
-    # return default range when datepicker is empty/cleared
-    elif start is None and end is None:
-        if 'isolation forest' in curr_title.lower():
-            print("5th")
-            graphs = create_fig(df_isoForest, 'isoForest')
-        elif 'k-means' in curr_title.lower():
-            print("6th")
-            graphs = create_fig(df_kmeans, 'kmeans')           
+    # # return default range when datepicker is empty/cleared
+    # elif start is None and end is None:
+    #     if 'isolation forest' in curr_title.lower():
+    #         print("7th")
+    #         graphs = create_fig(df_isoForest, 'isoForest')
+    #     elif 'auto-encoder' in curr_title.lower():
+    #         print("8th")
+    #         graphs = create_fig(df_autoEncoder, 'autoEncoder')  
+    #     elif 'k-means' in curr_title.lower():
+    #         print("9th")
+    #         graphs = create_cluster(df_kmeans, 'kmeans')           
     
     return graphs
